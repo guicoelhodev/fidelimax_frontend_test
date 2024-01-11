c61bf47805328d3483a64c48228880a08f8b0b11
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 17:24:14 -0300

    feat: create handle function for each star and control all update based on mouse position

--- src/components/StarQuestion/index.tsx
+++ src/components/StarQuestion/index.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { StarSvg } from './StarSvg'
+import React, { useState } from "react";
+import { StarSvg } from "./StarSvg";
 
-export const StarQuestion: React.FC = () => {
+type TStarQuestion = {
+  handleStars: (currentStarNumber: number) => void;
+  currentStar: number;
+}
+export const StarQuestion: React.FC<TStarQuestion> = ({ handleStars, currentStar }) => {
+  const [activeStar, setActiveStar] = useState(currentStar);
+
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 border border-red-500" onMouseLeave={() => handleStars(activeStar)}>
       {Array.from({ length: 5 }, (_, index) => (
-        <div  key={index}>
-          <StarSvg fill={index === 3 ? '#ffae00' : '#ACB1BA'}/>
+        <div key={index}>
+          <StarSvg
+            fill={index + 1 <= activeStar ? "#ffae00" : "#ACB1BA"}
+            starNumber={index + 1}
+            onMouseEnter={(starNumber) => setActiveStar(starNumber)}
+          />
         </div>
       ))}
     </div>
