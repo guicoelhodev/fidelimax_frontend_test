822643b14a98e3a8a066bf7d7656df4c5363f88b
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 12:23:26 -0300

    feat: create ClientForm component
    Try to get cached data

---
+++ src/app/components/ClientForm/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Response, useFormClientData } from '@/service/http/GET/useFormClientData'
+
+type TClientForm = {
+  formPreviousData: Response
+}
+export const ClientForm: React.FC<TClientForm> = ({ formPreviousData }) => {
+
+  const { data } = useFormClientData({ initialData: formPreviousData })
+
+  console.log('thats is the data', data);
+  return <div>
+    dsajhdsakjadshjk
+  </div>;
+}
