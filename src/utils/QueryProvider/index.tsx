62b9333c3db584fc5f23c8fd5bc12ac14b2e70c6
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 10:51:03 -0300

    feat: create queryProvider to use react query on application

---
+++ src/utils/QueryProvider/index.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode, useState } from "react";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+export const QueryProvider: FC<{ children: ReactNode }> = ({ children }) => {
+
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
