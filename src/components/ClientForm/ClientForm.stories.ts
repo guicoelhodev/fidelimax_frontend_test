4390c76c406a55b1088d07608ce2bdc59b72f516
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 16:00:24 -0300

    feat: create ClientForm stories for storybook

---
+++ src/components/ClientForm/ClientForm.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ClientForm } from '.'
+
+const meta = {
+  title: 'ClientForm',
+  component: ClientForm,
+  tags: [
+'autodocs'
+  ],
+  args: {
+    formPreviousData: null
+  }
+} satisfies Meta<typeof ClientForm>
+
+export default meta;
+
+type TStory = StoryObj<typeof meta>
+
+export const Primary : TStory = {}
