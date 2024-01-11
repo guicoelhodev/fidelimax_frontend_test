7a38278936c9637fb99315c23b98249d853f4262
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 12:20:38 -0300

    feat: add default formData to get on server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+import { Response } from '@/service/http/GET/useFormClientData'
+import { ClientForm } from './components/ClientForm';
+async function getData(): Promise<Response> {
+  const res = await fetch('https://fdlmx-backgrounds.sfo3.digitaloceanspaces.com/front-test/survey.json', { cache: 'force-cache'});
+  if(!res.ok) {
+    throw new Error('Failed to fetch data');
+  };
 
-export default function Home() {
+  const data = await res.json();
+  return data;
+}
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main>
       <h1 className='text-3xl text-blue-600'>Hello world</h1>
+      <ClientForm formPreviousData={data} />
     </main>
   )
 }
