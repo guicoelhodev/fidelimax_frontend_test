0a03b99a6de1d86f09ecfb2564fe8dfa955451a4
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 13:56:19 -0300

    refactor: remove useless request call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { Response } from '@/service/http/GET/useFormClientData'
+import { fidelimaxFormClientData } from '@/service/http/GET/useFormClientData'
 import { ClientForm } from './components/ClientForm';
-import { fetchWrapper } from '@/utils/fetchWrapper';
-async function getData(): Promise<Response> {
-  return await fetchWrapper<Response>('https://fdlmx-backgrounds.sfo3.digitaloceanspaces.com/front-test/survey.json');
-}
+
 export default async function Home() {
-  const data = await getData();
+  const data = await fidelimaxFormClientData();
 
   return (
     <main>
