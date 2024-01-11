6c8736edfea13e00c87ade488af99836ab9fd6ec
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 17:31:08 -0300

    refactor: add handleStar for StarGroup

--- src/components/ClientForm/index.tsx
+++ src/components/ClientForm/index.tsx
@@ -2,6 +2,7 @@
 
 import { Response, useFormClientData } from '@/hooks/GET/useFormClientData'
 import { StarQuestion } from '@/components/StarQuestion';
+import { useState } from 'react';
 
 type TClientForm = {
   formPreviousData: Response | null
@@ -9,9 +10,11 @@ type TClientForm = {
 export const ClientForm: React.FC<TClientForm> = ({ formPreviousData }) => {
 
   const { data } = useFormClientData({ initialData: formPreviousData})
+  const [activeStar, setActiveStar] = useState(0);
 
-  console.log('thats is the data', data);
-  return <div className='text-red-500'>
-    <StarQuestion />
+  // console.log('thats is the data', data);
+  // console.log(activeStar);
+  return <div className='text-red-500 flex flex-col items-start'>
+    <StarQuestion  handleStars={(star) => setActiveStar(star)} currentStar={activeStar} />
   </div>;
 }
