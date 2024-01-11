820409f75dec450c055362477191204e9077874f
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-11 14:34:29 -0300

    feat: create global setup to test all components with react-testing-library and vitest

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,30 @@
-import { defineConfig } from "vitest/config";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { URL, fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
-  test: {},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/__tests__/setup.ts',
+    coverage: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      exclude: ['**/mocks/**'],
+    },
+    include: ['./src/**/*.{test,spec}.{ts,tsx}'],
+    exclude: [...configDefaults.exclude, '**/mocks/**'],
+  },
 });
