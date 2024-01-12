bc1e370cb4aa383b2b4e652f09b4b9ac2dd70429
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-12 14:21:58 -0300

    test: add mock for RadioGroupQuestion

---
+++ src/components/UI/RadioGroupQuestion/mock/index.ts
@@ -0,0 +1,11 @@
+import { TClientQuestion } from "@/types/ClientForm";
+
+export const mockContent: TClientQuestion = {
+  content: "Texto mockado de exemplo",
+  typeQuestion: 2,
+  handleValue: () => {},
+  answerValue: 0,
+  mandatory: false,
+  horizontal: true,
+  itens: [],
+};
