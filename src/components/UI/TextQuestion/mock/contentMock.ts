36639b52a3407aaeb1a30fb8a9e7be2208399fea
Guilherme Santos Coelho <gs.coelho@outlook.com>
2024-01-12 15:29:18 -0300

    test: create unit test for TextQuestion component

---
+++ src/components/UI/TextQuestion/mock/contentMock.ts
@@ -0,0 +1,11 @@
+import { TClientQuestion } from "@/types/ClientForm";
+
+export const mockContent: TClientQuestion = {
+  content: "Texto mockado de exemplo",
+  typeQuestion: 3,
+  handleValue: () => {},
+  answerValue: '',
+  mandatory: false,
+  horizontal: true,
+  itens: [],
+};
