8e4ade63d97563d4fba262022015aa2eb040bbba
guicoelhodev <gs.coelho@outlook.com>
2024-01-13 15:11:25 -0300

    chore: add @testing-library/dom lib

--- src/components/ClientForm/index.tsx
+++ src/components/ClientForm/index.tsx
@@ -5,52 +5,58 @@ import { StarQuestion } from "@/components/UI/StarQuestion";
 import { useCallback, useState } from "react";
 import { RadioGroupQuestion } from "../UI/RadioGroupQuestion";
 import { TextQuestion } from "../UI/TextQuestion";
+import { SelectQuestion } from "../UI/SelectQuestion";
 
 type TClientForm = {
   formPreviousData: Response | null;
 };
 
 type TQuestionTypes = 1 | 2 | 3 | 4 | 5 | 6;
-type TFormValues = Record<TQuestionTypes, QuestionResponse>;
+
+type TFormValues = Map<TQuestionTypes, QuestionResponse>;
 
 export const ClientForm: React.FC<TClientForm> = ({ formPreviousData }) => {
   const getDefaultFormValues: () => TFormValues = useCallback(() => {
-    let tmpValues = {} as TFormValues;
+    let tmpValues = new Map() as TFormValues;
 
     formPreviousData?.itens.forEach((question) => {
-      tmpValues[question.typeQuestion as keyof typeof tmpValues] = question;
+      tmpValues.set(question.typeQuestion as TQuestionTypes, question);
     });
     return tmpValues;
-  }, [ formPreviousData?.itens ]);
+  }, []);
 
-  const [formValues, setFormValues] = useState<TFormValues>(getDefaultFormValues());
+  const [formValues, setFormValues] = useState<TFormValues>(
+    getDefaultFormValues()
+  );
 
+  const handleFormValues = (key: number, value: string | number | string[]) => {
+    const formKeyUpdated = {
+      ...formValues.get(key as TQuestionTypes)!,
+      answerValue: value,
+    } as QuestionResponse;
 
-  const handleFormValues = (key: number, value: string | number) => {
+    let formUpdated = new Map(formValues).set(
+      key as TQuestionTypes,
+      formKeyUpdated
+    );
 
-    const currentKeyValues = formValues[key as TQuestionTypes];
-    setFormValues((prevValues) => {
-      return {
-        ...prevValues,
-        [key]: {
-          ...currentKeyValues,
-          answerValue: value,
-        },
-      };  
-    })    
+    return setFormValues(formUpdated);
   };
+
+  // console.log(Array.from(formValues.values()));
 
   return (
     <div className="max-w-xl m-auto flex flex-col gap-2 items-start p-4 border border-red-200">
-
-      {Object.values(formValues).map((question) => {
+      {Array.from(formValues.values()).map((question) => {
         if (question.typeQuestion === 1) {
           return (
             <StarQuestion
               {...question}
               key={question.content}
               answerValue={question.answerValue}
-              handleValue={(star) => handleFormValues(question.typeQuestion, star)}
+              handleValue={(star) =>
+                handleFormValues(question.typeQuestion, star)
+              }
             />
           );
         }
@@ -60,16 +66,36 @@ export const ClientForm: React.FC<TClientForm> = ({ formPreviousData }) => {
             <RadioGroupQuestion
               {...question}
               key={question.typeQuestion}
-              handleValue={(value) => handleFormValues(question.typeQuestion, value)}
+              handleValue={(value) =>
+                handleFormValues(question.typeQuestion, value)
+              }
               answerValue={question.answerValue}
             />
           );
         }
 
-        if(question.typeQuestion === 3) {
+        if (question.typeQuestion === 3) {
           return (
-            <TextQuestion {...question} key={question.content} handleValue={(value) => handleFormValues(question.typeQuestion, value)} />
-          )
+            <TextQuestion
+              {...question}
+              key={question.content}
+              handleValue={(value) =>
+                handleFormValues(question.typeQuestion, value)
+              }
+            />
+          );
+        }
+
+        if (question.typeQuestion === 4) {
+          return (
+            <SelectQuestion
+              {...question}
+              key={question.content}
+              handleValue={(value) =>
+                handleFormValues(question.typeQuestion, value)
+              }
+            />
+          );
         }
       })}
     </div>
